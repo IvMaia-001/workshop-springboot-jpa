c2dd1362a8cc9c0cec5829c2cc3e2e8af4145d60
Ivan Maia <ivanleonardo_maia@hotmail.com>
2023-09-29 17:39:59 -0300

    Product

--- src/main/java/com/aulajava/course/entities/User.java
+++ src/main/java/com/aulajava/course/entities/User.java
@@ -3,7 +3,6 @@ package com.aulajava.course.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -94,7 +93,10 @@ public class User implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -106,7 +108,12 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}	
 
 }
